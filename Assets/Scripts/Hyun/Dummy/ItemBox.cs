6bfebf4586f7717c48145484fd510c0c1ef92aaa
Hyun <oiwer23@gmail.com>
2025-12-19 12:59:10 +0900

    [Add]아이템 박스 (일단 보류 상태)

---
+++ Assets/Scripts/Hyun/Dummy/ItemBox.cs
@@ -0,0 +1,34 @@
+using System.Xml;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    [SerializeField] private GameObject itemPickupPrefab; //드랍될 아이템 프리팹
+    [SerializeField] private string _uniqueID;
+
+    private bool _isOpened = false;
+    private void OnValidate()
+    {
+        if (UnityEditor.PrefabUtility.IsPartOfPrefabAsset(this))
+        {
+            _uniqueID = string.Empty;
+            return;
+        }
+
+        if (string.IsNullOrEmpty(_uniqueID))
+        {
+            _uniqueID = System.Guid.NewGuid().ToString();
+            UnityEditor.EditorUtility.SetDirty(this);
+        }
+    }
+    public void OpenBox()
+    {
+        if (_isOpened)
+        {
+            return;
+        }
+        _isOpened = true;
+        Instantiate(itemPickupPrefab, transform.position, Quaternion.identity);
+    }
+
+}
