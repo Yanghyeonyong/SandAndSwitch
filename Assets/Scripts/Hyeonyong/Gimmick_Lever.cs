3959c83b9e3532b7b87650e6802059c3f57e288b
Yanghyeonyong <didgusdyd2@gmail.com>
2025-12-19 14:38:00 +0900

    [Add] 레버 에셋 적용

--- Assets/Scripts/Hyeonyong/Gimmick_Lever.cs
+++ Assets/Scripts/Hyeonyong/Gimmick_Lever.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class Gimmick_Lever : Gimmick
 {
     [SerializeField] bool _turnLever=false;
-
     Gimmick_Object _obj;
+    Animator _animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
         if (CheckClear())
         {
             
@@ -22,10 +22,12 @@ public class Gimmick_Lever : Gimmick
         if (_turnLever)
         {
             _obj.TurnOn();
+            _animator.SetTrigger("TurnOn");
         }
         else
         {
             _obj.TurnOff();
+            _animator.SetTrigger("TurnOff");
         }
     }
 }
--- Assets/Scripts/Hyeonyong/Shutter.cs
+++ Assets/Scripts/Hyeonyong/Shutter.cs
@@ -3,13 +3,20 @@ using UnityEngine;
 public class Shutter : Gimmick_Object
 {
     [SerializeField] GameObject _shutterObject;
-    
+    Animator _animator;
+    private void Start()
+    {
+        _animator = _shutterObject.GetComponent<Animator>();
+    }
+
     public override void TurnOn()
     {
-        _shutterObject.transform.localPosition += new Vector3(0f,5f,0f);
+        //_shutterObject.transform.localPosition += new Vector3(0f,5f,0f);
+        _animator.SetTrigger("TurnOn");
     }
     public override void TurnOff()
     {
-        _shutterObject.transform.localPosition += new Vector3(0f, -5f, 0f);
+        //_shutterObject.transform.localPosition += new Vector3(0f, -5f, 0f);
+        _animator.SetTrigger("TurnOff");
     }
 }
