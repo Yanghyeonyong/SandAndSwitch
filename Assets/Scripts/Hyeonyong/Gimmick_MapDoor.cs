dbac4a502f12f45094895d916214718fbc5f6557
Yanghyeonyong <didgusdyd2@gmail.com>
2025-12-23 18:02:27 +0900

    [Edit] 문 양방향 이동 수정

--- Assets/Scripts/Hyeonyong/Gimmick_MapDoor.cs
+++ Assets/Scripts/Hyeonyong/Gimmick_MapDoor.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class Gimmick_MapDoor : Gimmick
 {
-    [SerializeField] Transform _teleportPos;
+    [SerializeField] GameObject _teleportPos;
+    InteractiveObject _interactObj;
+    //[SerializeField] GameObject _interactiveUI;
+    private void Start()
+    {
+        _interactObj =_teleportPos.GetComponent<InteractiveObject>();
+    }
     public override void StartGimmick()
     {
-        GameManager.Instance._player.transform.position= _teleportPos.position;
+
+        GameManager.Instance._player.transform.position= _teleportPos.transform.position;
+        StartCoroutine( _interactObj.Teleport());
+        //_interactiveUI.SetActive(true);
     }
 
 
