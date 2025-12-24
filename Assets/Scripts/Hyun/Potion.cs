30a0e03485efee98516cba7ff37b5c09ebcf1bd9
Hyun <oiwer23@gmail.com>
2025-12-24 16:37:49 +0900

    [Edit] 물약 미사용 코드 수정

--- Assets/Scripts/Hyun/Potion.cs
+++ Assets/Scripts/Hyun/Potion.cs
@@ -6,12 +6,8 @@ public class Potion : MonoBehaviour
     [SerializeField] private ItemData _itemData;
     //[SerializeField] private AudioSource _audio;//회복사운드를 따로 넣을것이라면..
 
-    //포션 사용을 알리는 이벤트
-    public static event Action OnUsePotion;
-
     public void UsePotion()
     {
-        OnUsePotion?.Invoke();//물약 사용을 알리는 이벤트 발생
         GameManager.Instance.PlayerHeal(1);
     }
 }
